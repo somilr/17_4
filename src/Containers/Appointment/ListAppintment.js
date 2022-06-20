f613104c8d348a976916a1e19b6a03718683556d
somilr <ribadiyasomil@gmail.com>
2022-06-19 20:31:04 -0700

    20_6

--- src/Containers/Appointment/ListAppintment.js
+++ src/Containers/Appointment/ListAppintment.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function ListAppintment() {
+  const [data, setData] = useState([]);
+
   return (
-    <div>ListAppintment</div>
+<section id="appointment" classname="appointment">
+  <div classname="container">
+    <div classname="section-title">
+      <h2>Make an Appointment</h2>
+      <p>Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc aliquam eget nibh eu euismod. Donec dapibus
+        blandit quam volutpat sollicitudin. Fusce tincidunt sit amet ex in volutpat. Donec lacinia finibus tortor.
+        Curabitur luctus eleifend odio. Phasellus placerat mi et suscipit pulvinar.</p>
+    </div>
+    <div classname="row ">
+      <div classname="col-6 text-center ">
+        <NavLink to={"/BookAppointment"}>BookAppoinment</NavLink>
+      </div>
+      <div classname="col-6">
+       <NavLink to={"/ListAppointment"}>List Appointment </NavLink>
+      </div>
+    </div>
+  </div>
+  <div>
+    
+  </div>
+</section>
+
+
   )
 }
